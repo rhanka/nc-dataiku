32f36a64b8407e1a2870c794617d83b86d36f989
rhanka <fabien.antoine@m4x.org>
2024-12-23 11:12:17 +0000

    Saved web app Synth-data-API (NONCONFORMITIES.3DGvs3v)

--- web_apps/3DGvs3v/backend.py
+++ web_apps/3DGvs3v/backend.py
@@ -17,6 +17,7 @@ def first_call():
     mydataset = dataiku.Dataset("NC_types_random_500_final_structured")
     mydataset_df = mydataset.get_dataframe(sampling='head', limit=max_rows)
 
+    mydataset_df['nc_event_date'] = mydataset_df['nc_event_date'].astype(str)
     # Pandas dataFrames are not directly JSON serializable, use to_json()
     data = mydataset_df.to_dict(orient='records')
     return json.dumps({"status": "ok", "data": data})
