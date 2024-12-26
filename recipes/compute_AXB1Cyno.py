dc0ef5458af6e8d5ab67d884cf11468ce49f7ac5
rhanka <fabien.antoine@m4x.org>
2024-12-26 21:53:12 +0000

    Recipe NONCONFORMITIES.compute_AXB1Cyno saved through notebook notebook editor for compute_AXB1Cyno

--- recipes/compute_AXB1Cyno.py
+++ recipes/compute_AXB1Cyno.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 import dataiku
 import os
-import markitdown
+from markitdown import MarkItDown
+import tempfile
+
+md = MarkItDown()
 
 # Folders
 A220_tech_docs = dataiku.Folder("SoQWOnhR")          # Input folder
@@ -15,25 +18,20 @@ for pdf_file in pdf_files:
     # Lire le contenu PDF
     with A220_tech_docs.get_download_stream(pdf_file) as f:
         pdf_data = f.read()
+        
+    # Utiliser un fichier temporaire pour la conversion
+    with tempfile.NamedTemporaryFile(delete=True, suffix=".pdf") as temp_pdf:
+        temp_pdf.write(pdf_data)
+        temp_pdf.flush()  # Assurez-vous que le contenu est écrit sur le disque
 
-    # Convertir en Markdown
-    md_content = markitdown.convert(pdf_data)
+        # Convertir en Markdown
+        md_content = md.convert(temp_pdf.name)
 
-    # Écrire le fichier .md
-    md_file_name = os.path.splitext(pdf_file)[0] + ".md"
-    with A220_tech_docs_prep.get_writer(md_file_name) as writer:
-        writer.write(md_content)
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-import importlib.metadata
-
-for dist in importlib.metadata.distributions():
-    if dist.metadata['Name'] == "markitdown":
-        print(f"{dist.metadata['Name']}=={dist.version}")
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-print(dir(markitdown))
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-import markitdown
-print(dir(markitdown))
+        # Afficher le nombre de lignes dans le contenu Markdown
+        num_lines = len(md_content.text_content.splitlines())
+        print(f"Nombre de lignes : {num_lines}")
+        
+        # Écrire le fichier .md
+        md_file_name = os.path.splitext(pdf_file)[0] + ".md"
+        with A220_tech_docs_prep.get_writer(md_file_name) as writer:
+            writer.write(md_content.text_content.encode('utf-8'))
