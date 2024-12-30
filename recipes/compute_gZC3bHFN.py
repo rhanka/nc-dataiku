f7020da8bf4cfbe4dbf3df24a59a85fa22607d22
rhanka <fabien.antoine@m4x.org>
2024-12-30 14:21:29 +0000

    Saved recipe: NONCONFORMITIES.compute_gZC3bHFN

--- recipes/compute_gZC3bHFN.py
+++ recipes/compute_gZC3bHFN.py
@@ -8,8 +8,19 @@ NC_types_random_500_md_concat = dataiku.Dataset("NC_types_random_500_md_concat")
 NC_types_random_500_md_concat_df = NC_types_random_500_md_concat.get_dataframe()
 
 
-
-
 # Write recipe outputs
 NC_types_500_md_files = dataiku.Folder("gZC3bHFN")
 NC_types_500_md_files_info = NC_types_500_md_files.get_info()
+
+
+# Parcourir chaque ligne du DataFrame et écrire les fichiers Markdown
+for _, row in NC_types_random_500_md_concat_df.iterrows():
+    doc_id = row["doc"]  # Identifiant pour le fichier
+    markdown_content = row["chunk_concat"]  # Contenu Markdown
+    
+    # Définir le nom du fichier
+    file_name = f"{doc_id}.md"
+    
+    # Écrire dans le dossier de sortie
+    with NC_types_500_md_files.get_writer(file_name) as writer:
+        writer.write(markdown_content.encode("utf-8"))
