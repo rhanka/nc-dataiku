dedcab4a076f0067ca9ba3a046c0cf2e6afd01a1
rhanka <fabien.antoine@m4x.org>
2024-12-30 23:00:28 +0000

    Saved web app Synth-data-API (NONCONFORMITIES.3DGvs3v)

--- web_apps/3DGvs3v/backend.py
+++ web_apps/3DGvs3v/backend.py
@@ -24,6 +24,14 @@ completion = llm.new_completion()
 
 langchain_llm = DKUChatLLM(llm_id=LLM_ID)
 
+@app.before_request
+def log_request_info():
+    app.logger.debug(f"Request method: {request.method}")
+    app.logger.debug(f"Request URL: {request.url}")
+    if request.json:
+        app.logger.debug(f"Request JSON body: {request.json}")
+    else:
+        app.logger.debug("No JSON body in the request.")
 
 @app.route('/nc')
 def non_conformities():
