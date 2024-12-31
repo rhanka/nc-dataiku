8e1050664cce4791361d1117b0a6efc7c3556d49
rhanka <fabien.antoine@m4x.org>
2024-12-31 20:54:19 +0000

    Saved web app Synth-data-API (NONCONFORMITIES.3DGvs3v)

--- web_apps/3DGvs3v/backend.py
+++ web_apps/3DGvs3v/backend.py
@@ -2,25 +2,11 @@ import dataiku
 import pandas as pd
 from flask import request
 from flask_cors import CORS
-
-CORS(app, resources={r"/*": {"origins": "https://svelte.dev"}})
-
+import json
+from langchain.chains.question_answering import load_qa_chain
 from dataiku.langchain.dku_llm import DKULLM, DKUChatLLM
 
-LLM_ID = "retrievalaugmented:8jpMQW44:gpt-4o-mini-a220-all-sources"
-LLM_ID = "retrievalaugmented:WnKb6p17:gpt-4o-mini-a220-nc"
-LLM_ID = "retrievalaugmented:zQ92IhQ9:gpt-4o-mini-a220-tech-docs"
-
-# Create a handle for the LLM of your choice
-client = dataiku.api_client()
-project = client.get_default_project()
-llm = project.get_llm(LLM_ID)
-
-# Create and run a completion query
-completion = llm.new_completion()
-
-langchain_llm = DKUChatLLM(llm_id=LLM_ID)
-
+CORS(app, resources={r"/*": {"origins": "https://svelte.dev"}})
 
 @app.route('/nc')
 def non_conformities():
@@ -46,6 +32,37 @@ def non_conformities():
     
     return json.dumps(data)
 
+#LLM_ID = "retrievalaugmented:8jpMQW44:gpt-4o-mini-a220-all-sources"
+#LLM_ID = "retrievalaugmented:WnKb6p17:gpt-4o-mini-a220-nc"
+#LLM_ID = "retrievalaugmented:zQ92IhQ9:gpt-4o-mini-a220-tech-docs"
+LLM_ID = "openai:OpenAI-FA:gpt-4o-mini"
+KB_IDs = {
+    "tech_docs": "zQ92IhQ9",
+    "non_conformities": "WnKb6p17"
+}
+
+# Create a handle for the LLM of your choice
+client = dataiku.api_client()
+project = client.get_default_project()
+llm = project.get_llm(LLM_ID)
+
+
+
+# Preparing the Knowledge Bank, Vector store and LLM
+KBs = {
+    key: dataiku.KnowledgeBank(id=value, project_key=project.project_key)
+    for key, value in KB_IDs.items()
+}
+vector_stores = {
+    key: value.as_langchain_vectorstore()
+    for key, value in KBs.items()
+}
+# Create and run a completion query
+completion = llm.new_completion()
+
+langchain_llm = DKUChatLLM(llm_id=LLM_ID, temperature=0)
+chain = load_qa_chain(langchain_llm, chain_type="stuff")
+
 @app.route('/ai', methods=['POST'])
 def ai():
     app.logger.info("Handling /ai endpoint.")
@@ -55,6 +72,42 @@ def ai():
     # Vérifier que le champ "messages" est présent
     if not data or "messages" not in data:
         return json.dumps({"error": "Invalid input: 'messages' field is required."}), 400
+
+    
+    # 1s step: expand query
+    prompt = (
+        f"You're supporting Quality Controller for A220 and rely on the knowledge from the A220 technical "
+        f"doc and non conformity knowledge base (vector databases). You must provide an optimized expanded prompt towards "
+        f"those vector databases to enable the best retrieval given the user input. "
+        f"The expansion should only concern specificity around the user query and avoid retrieval of non specific vocabulary, "
+        f"as knowledge databses will contain any past non conformity. Avoid generic vocabulary like 'non-conformity', 'issue', "
+        f"'specification', 'standard', 'operations', 'maintainance'. But expand domain vocabulary.\n "
+        f"Format of the output: Please just provide the query without any comment to be reused as is. "
+        f"Optimal request should be between 20 and 50 words \n\n"
+        f"The user is the following:\n {user_message}\n\n\n"
+        f"Remember to only provide the requested query for the knowledge database without any comment."
+    )
+    llm = project.get_llm(LLM_ID)
+    completion = llm.new_completion()
+    completion.with_message(prompt)
+    resp = completion.execute()
+        
+    if resp.success:
+        # 2nd step : gather documents
+        query = resp.text
+        search_results = [
+            result
+            for key, value in vector_stores.items() 
+            for result in value.similarity_search(query)
+        ]
+        search_results = [ {
+                "doc": s.metadata['doc'],
+                "chunk_id": s.metadata['chunk_id'],
+                #"chunk": s.page_content
+            }
+            for s in search_results
+        ]
+print(search_results)
 
     # Récupérer le dernier message utilisateur (assumé en dernier dans l'historique)
     messages = data["messages"]
@@ -78,6 +131,10 @@ def ai():
     completion.with_message(prompt)
     resp = completion.execute()
 
+    if resp.success:
+        try:
+            
+    
     # Vérifier le succès de la réponse
     if resp.success:
         try:
