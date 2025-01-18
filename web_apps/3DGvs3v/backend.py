9253cfcf1530043978594e750056cfabbbb3dde2
fabien.antoine@cgi.com <fabien.antoine@m4x.org>
2025-01-18 00:17:10 +0000

    Saved web app Synth-data-API (NONCONFORMITIES.3DGvs3v)

--- web_apps/3DGvs3v/backend.py
+++ web_apps/3DGvs3v/backend.py
@@ -234,8 +234,7 @@ def ai():
                         query = chunk.data['trace']['children'][1]['outputs']['text']
                         text = json.dumps({'text': chunk.data['text'].replace('\n', '\\n')})
                         app.logger.info(query)
-                        yield f"data: {text}\n\n"  # Indicateur de fin de la première étape
-                        yield f"data: [DONE]\n\n"  # Indicateur de fin de la première étape
+                        yield f"data: {text}\n\ndata: [DONE]\n\n"  # Indicateur de fin de la première étape
                 
                 # 2nd step : gather documents relative to query
                 sources = {
