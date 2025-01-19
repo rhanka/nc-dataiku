8ae2db9165e705d27ed99f3a36484262874ad815
fabien.antoine@cgi.com <fabien.antoine@m4x.org>
2025-01-19 03:18:08 +0000

    Saved web app Synth-data-API (NONCONFORMITIES.3DGvs3v)

--- web_apps/3DGvs3v/backend.py
+++ web_apps/3DGvs3v/backend.py
@@ -177,7 +177,7 @@ def format_data_stream(type, input,metadata):
 
 def stream_prompt_recipe(recipe_name, inputs):
     agent_name = next((agent for agent, recipe in agents.items() if recipe == recipe_name), recipe_name)
-    yield format_data_stream("action",agent_name,agent_name)
+    yield format_data_stream("action",agentsMsg[agent_name],agent_name)
     result = None
     for chunk in completion_from_prompt_recipe(recipe_name, inputs).execute_streamed():
         if isinstance(chunk, DSSLLMStreamedCompletionChunk):
@@ -283,12 +283,12 @@ def ai():
                 
                 # 2nd step : gather documents relative to query
                 app.logger.info("nc_search")
-                yield format_data_stream("action","nc_search","nc_search")
+                yield format_data_stream("action",agentsMsg["nc_search"],"nc_search")
                 non_conformities = exec_prompt_recipe(agents["nc_search"], {"input": query})
                 yield format_data_stream("result",non_conformities,"nc_search")
                 
                 app.logger.info("doc_search")
-                yield format_data_stream("action","doc_search","doc_search")
+                yield format_data_stream("action",agentsMsg["doc_search"],"doc_search")
                 tech_docs = exec_prompt_recipe(agents["doc_search"], {"input": query})
                 yield format_data_stream("result",tech_docs,"doc_search")
                 
