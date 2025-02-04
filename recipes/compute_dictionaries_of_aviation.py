8d3af2a7cbd30288f75a18fd54f0b3f7ad3ff6b4
ludovic.bocken@cgi.com <ludovic.bocken@cgi.com>
2025-02-04 20:43:07 +0000

    Recipe NONCONFORMITIES.compute_dictionaries_of_aviation saved through notebook notebook editor for compute_dictionaries_of_aviation

--- recipes/compute_dictionaries_of_aviation.py
+++ recipes/compute_dictionaries_of_aviation.py
@@ -1,20 +1,35 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
+import fitz  # PyMuPDF
+import pandas as pd
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 Dictionaries = dataiku.Folder("PP5gsWW1")
 Dictionaries_info = Dictionaries.get_info()
 
+# Initialize an empty list to store data
+data = []
 
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+# Iterate through the files in the folder
+for file_path in Dictionaries.list_paths_in_partition():
+    with Dictionaries.get_download_stream(file_path) as f:
+        # Open the PDF file
+        pdf_document = fitz.open(stream=f.read(), filetype="pdf")
+        text = ""
+        for page_num in range(len(pdf_document)):
+            page = pdf_document[page_num]
+            text += page.get_text()
+        data.append({"file_path": file_path, "content": text})
 
-dictionaries_of_aviation_df = ... # Compute a Pandas dataframe to write into dictionaries_of_aviation
+# Convert the list to a dataframe
+dictionaries_of_aviation_df = pd.DataFrame(data)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+dictionaries_of_aviation_df
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 dictionaries_of_aviation = dataiku.Dataset("dictionaries_of_aviation")
 dictionaries_of_aviation.write_with_schema(dictionaries_of_aviation_df)
