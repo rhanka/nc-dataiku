3051f5d9c21c66b5a981947bd7d2c822c31318f0
ludovic.bocken@cgi.com <ludovic.bocken@cgi.com>
2025-02-05 19:27:57 +0000

    Recipe NONCONFORMITIES.compute_transport_canada_dic saved through notebook notebook editor for compute_transport_canada_dic

--- recipes/compute_transport_canada_dic.py
+++ recipes/compute_transport_canada_dic.py
@@ -20,20 +20,41 @@ dictionaries_of_aviation_df = dictionaries_of_aviation.get_dataframe()
 dictionaries_of_aviation_df
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-dictionary = dictionaries_of_aviation_df["content"][2]
+dictionary = dictionaries_of_aviation_df["content"][1]
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+dictionary
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+pattern = r'\n(\w+)\s\n'
+entries = re.findall(pattern, dictionary)
+entries
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+pattern = r'\n(\w+)\s\n'
+replacement = r'[[[\1]]] /'
+dictionary_with_entries = re.sub(pattern, replacement, dictionary)
+dictionary_with_entries
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Split the text using triple brackets
+entries = re.split(r'\[\[\[|\]\]\]', dictionary_with_entries)
 
+# Remove empty entries and strip whitespace
+entries = [entry.strip() for entry in entries if entry.strip()]
+# Ensure both columns have the same length by adding empty strings if necessary
+if len(entries) % 2 != 0:
+    entries.append('')
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# Create a DataFrame with two columns: 'Entry' and 'Definition'
+data = {'Entry': entries[0::2], 'Definition': entries[1::2]}
+df = pd.DataFrame(data)
+df
 
-transport_canada_dic_df = dictionaries_of_aviation_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+transport_canada_dic_df = pd.DataFrame({"entry":list(df["Definition"][0:1870]),"definition":list(df["Entry"][1:1871])})
 
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 transport_canada_dic = dataiku.Dataset("transport_canada_dic")
 transport_canada_dic.write_with_schema(transport_canada_dic_df)
