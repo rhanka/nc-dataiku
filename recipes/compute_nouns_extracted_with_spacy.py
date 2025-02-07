5d67fb7bf6ff2b876752d06826157f30f995bae0
ludovic.bocken@cgi.com <ludovic.bocken@cgi.com>
2025-02-07 12:57:31 +0000

    Recipe NONCONFORMITIES.compute_nouns_extracted_with_spacy saved through notebook notebook editor for compute_nouns_extracted_with_spacy

--- recipes/compute_nouns_extracted_with_spacy.py
+++ recipes/compute_nouns_extracted_with_spacy.py
@@ -1,27 +1,3 @@
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
-
-# Read recipe inputs
-corpus_for_knowledge_engineering = dataiku.Dataset("corpus_for_knowledge_engineering")
-corpus_for_knowledge_engineering_df = corpus_for_knowledge_engineering.get_dataframe()
-
-
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-nouns_extracted_with_spacy_df = corpus_for_knowledge_engineering_df # For this sample code, simply copy input to output
-
-
-# Write recipe outputs
-nouns_extracted_with_spacy = dataiku.Dataset("nouns_extracted_with_spacy")
-nouns_extracted_with_spacy.write_with_schema(nouns_extracted_with_spacy_df)
-
-
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
 # # Import libraries
 
@@ -31,6 +7,7 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 import spacy
+from dframcy import DframCy
 
 # Load the spaCy model
 nlp = spacy.load("en_core_web_sm")
@@ -49,12 +26,42 @@ corpus_for_knowledge_engineering_df["upper_text"] = corpus_for_knowledge_enginee
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 corpus_for_knowledge_engineering_df
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+def extract_nouns_with_spacy(text):
+    dframcy = DframCy(nlp)
+    doc = dframcy.nlp(text)
+    pos_df = dframcy.to_dataframe(doc)
+    tags = ["NN", "NNP"]
+    filtered_pos_df = pos_df[pos_df["token_tag_"].isin(tags)]
+    return filtered_pos_df
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Assuming you have already defined your extract_nouns_with_spacy function
+def extract_nouns_with_spacy(text):
+    nlp.max_length = 3000000  # Set this to a value higher than your text length
+    dframcy = DframCy(nlp)
+    doc = dframcy.nlp(text)
+    pos_df = dframcy.to_dataframe(doc)
+    tags = ["NN", "NNP"]
+    filtered_pos_df = pos_df[pos_df["token_tag_"].isin(tags)]
+    return filtered_pos_df
+
+# Apply the function to the 'text' column and consolidate the results
+nouns_extracted_with_spacy_df = corpus_for_knowledge_engineering_df['text'].apply(extract_nouns_with_spacy)
+
+# If you want to concatenate all the results into a single dataframe
+consolidated_results_df = pd.concat(nouns_extracted_with_spacy_df.tolist(), ignore_index=True)
+
+print(consolidated_results_df)
+
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
 # # Extract nouns
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Define the function to extract nouns and return a DataFrame
 def extract_nouns(doc_id, text):
+    # Increase the max_length limit
+    nlp.max_length = 3000000  # Set this to a value higher than your text length
     doc = nlp(text)
     nouns = [(doc_id, token.text, token.pos_) for token in doc if token.pos_ == "NOUN"]
     return pd.DataFrame(nouns, columns=['doc_id', 'word', 'tag'])
@@ -94,3 +101,8 @@ merged_pos_df
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
 # ## Upper case
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Write recipe outputs
+nouns_extracted_with_spacy = dataiku.Dataset("nouns_extracted_with_spacy")
+nouns_extracted_with_spacy.write_with_schema(nouns_extracted_with_spacy_df)
